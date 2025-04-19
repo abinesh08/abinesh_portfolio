178f13f1f2f883578e96ab58ad55fba341eb8fae
Abinesh R <74003839+abinesh08@users.noreply.github.com>
2025-04-19 14:13:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function updateDateTime() {
+  const now = new Date();
+  const datetime = now.toLocaleString();
+  document.getElementById("datetime").textContent = `Last updated: ${datetime}`;
+}
+updateDateTime();
+
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 200) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
